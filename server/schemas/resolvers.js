45e72ed1fb84d9cf18c79ad2882cb60a67249295
Lindsey Lansford <lindsey.lansford@gmail.com>
2022-09-24 19:05:34 -0600

    making a change to the resolver

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,14 +1,15 @@
-const { AuthenticationError } = require("apollo-server-express");
 const { User } = require("../models");
+const { AuthenticationError } = require("apollo-server-express");
 const { signToken } = require("../utils/auth");
 
 const resolvers = {
   Query: {
     me: async (parent, args, context) => {
       if (context.user) {
-        return User.findOne({ _id: context.user._id }).populate("profile");
+        const user = await User.findOne({ _id: context.user._id });
+        return user;
       }
-      throw new AuthenticationError("You need to be logged in!");
+      // throw new AuthenticationError("You need to be logged in!");
     },
   },
 
@@ -38,28 +39,22 @@ const resolvers = {
         const updatedUser = await User.findOneAndUpdate(
           { _id: context.user._id },
           { $addToSet: { savedBooks: book } },
-          {
-            new: true,
-            runValidators: true,
-          }
-        );
-        return updatedUser;
-      }
-      throw new AuthenticationError("You need to be logged in!");
-    },
-
-    removeBook: async (parent, { book }, context) => {
-      if (context.user) {
-        const updatedUser = await User.findOneAndUpdate(
-          { _id: context.user._id },
-          { $pull: { savedBooks: { bookId: book.bookId } } },
           { new: true }
         );
         return updatedUser;
       }
-      throw new AuthenticationError("You need to be logged in!");
+      // throw new AuthenticationError("You need to be logged in!");
     },
-  }
+
+    removeBook: async (parent, { bookId }, context) => {
+      const updatedUser = await User.findOneAndUpdate(
+        { _id: context.user._id },
+        { $pull: { savedBooks: { bookId: bookId } } },
+        { new: true }
+      );
+      return updatedUser;
+    },
+  },
 };
 
 module.exports = resolvers;
